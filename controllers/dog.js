52724ba4ab03701db181d52d12da67924fb6fd28
jaytai2040 <jayyr2040@gmail.com>
2021-07-24 15:27:42 +0800

    feat: 🎸 Test User form and password encrypt(works before form)

--- controllers/dog.js
+++ controllers/dog.js
@@ -12,6 +12,38 @@ router.get("/", (req, res) => {
   });
 });
 
+// SEED
+router.get("/seed", (req, res) => {
+  Dog.remove({}, (error, dogs) => {
+    Dog.create([{
+      "name": "Kai Kai",
+      "breed": "Pomeranian",
+      "image": "https://images.pexels.com/photos/5766194/pexels-photo-5766194.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+      "sex": "M",
+      "yob": 2019,
+      "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+    },
+    {
+      "name": "Money",
+      "breed": "Daschund",
+      "image": "https://images.unsplash.com/photo-1612195665612-30c359370759?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80",
+      "sex": "F",
+      "yob": 2017,
+      "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+    },
+    {
+      "name": "Heroku",
+      "breed": "Saint Bernard",
+      "image": "https://images.unsplash.com/photo-1562193882-0ea2da14e6e3?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=967&q=80",
+      "sex": "F",
+      "yob": 2019,
+      "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+    }], (err, data) => {
+      res.redirect("/dogs");
+    });
+  });
+});
+
 // CREATE
 router.post("/", (req, res) => {
   Dog.create(req.body, (error, createdDog) => {
@@ -58,36 +90,6 @@ router.get("/:name", (req, res) => {
       } )
 });
 
-// SEED
-router.get("/seed", (req, res) => {
-  Dog.remove({}, (error, dogs) => {
-    Dog.create([{
-      "name": "Kai Kai",
-      "breed": "Pomeranian",
-      "image": "https://images.pexels.com/photos/5766194/pexels-photo-5766194.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
-      "sex": "M",
-      "yob": 2019,
-      "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
-    },
-    {
-      "name": "Money",
-      "breed": "Daschund",
-      "image": "https://images.unsplash.com/photo-1612195665612-30c359370759?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80",
-      "sex": "F",
-      "yob": 2017,
-      "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
-    },
-    {
-      "name": "Heroku",
-      "breed": "Saint Bernard",
-      "image": "https://images.unsplash.com/photo-1562193882-0ea2da14e6e3?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=967&q=80",
-      "sex": "F",
-      "yob": 2019,
-      "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
-    }], (err, data) => {
-      res.redirect("/dogs");
-    });
-  });
-});
+
 
 module.exports = router;
--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 const mongoose = require("mongoose");
 const MONGODB_URI = process.env.MONGODB_URI;
+const session = require("express-session");
 
 // Mongoose connection
 mongoose.connection.on("error", (err) =>
@@ -23,15 +24,25 @@ mongoose.connection.once("open", () => {
 });
 
 // Middleware
+app.use(express.urlencoded({ extended: false })); 
 app.use(express.json());
+app.use(
+  session({
+    secret: process.env.SECRET, 
+    resave: false, 
+    saveUninitialized: false,
+  })
+);
 
 // Controllers
 const usersController = require("./controllers/user");
 const dogsController = require("./controllers/dog");
-const likeEventsController = require("./controllers/likeEvent")
-app.use("/users", usersController)
-app.use("/dogs", dogsController)
-app.use("/likeevents", likeEventsController)
+const likeEventsController = require("./controllers/likeEvent");
+const sessionsController = require("./controllers/sessions.js");
+app.use("/users", usersController);
+app.use("/dogs", dogsController);
+app.use("/likeevents", likeEventsController);
+app.use("/sessions", sessionsController)
 
 app.listen(PORT, () => {
   console.log("Matching happening on port", PORT);
